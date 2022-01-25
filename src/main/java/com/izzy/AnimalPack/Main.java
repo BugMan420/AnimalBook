05743f5c19ad195be0a7ed59a2c2c0c96695e3ef
BugMan420 <ismaelsim@hotmail.co.uk>
2022-01-25 08:44:08 +0000

    Fixed dogs age not updating

--- src/main/java/com/izzy/AnimalPack/Main.java
+++ src/main/java/com/izzy/AnimalPack/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String args[]) {
         Dog Poodle = new Dog(11,5);
         Poodle.name = "Fluffy";
+        Poodle.age = 5;
         Poodle.dogName();
         Poodle.eat("turkey");
         Poodle.setAge();
--- src/test/java/com/izzy/AnimalPackTest/AgeTest.java
+++ src/test/java/com/izzy/AnimalPackTest/AgeTest.java
@@ -9,6 +9,8 @@ public class AgeTest {
     @Test
     public void testAge(){
         Dog testDog = new Dog(2,2);
-        assertEquals(2, testDog.getSize(), "The expected weight of the dog was wrong");
+        testDog.age = 5;
+        testDog.setAge();
+        assertEquals(2, testDog.getAge(), "The expected age of the dog was wrong");
     }
 }
