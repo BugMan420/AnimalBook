af06e747a1f2c6b6f6e81e322e42a178183705c4
BugMan420 <ismaelsim@hotmail.co.uk>
2022-01-25 14:26:08 +0000

    commented out main to test coverage

--- src/main/java/com/izzy/AnimalPack/Main.java
+++ src/main/java/com/izzy/AnimalPack/Main.java
@@ -2,7 +2,7 @@ package com.izzy.AnimalPack;
 
 public class Main {
     public static void main(String args[]) {
-        Dog Poodle = new Dog(5, 5);
+        /*Dog Poodle = new Dog(5, 5);
         Poodle.setName("Bob");
         Poodle.printName();
         System.out.println(Poodle.getSize());
@@ -12,6 +12,6 @@ public class Main {
         String poop = Poodle.excretion();
         String breathing = Poodle.breathing();
         String reproduction = Poodle.reproduction();
-        System.out.println(breathing + poop + reproduction );
+        System.out.println(breathing + poop + reproduction );*/
     }
 }
