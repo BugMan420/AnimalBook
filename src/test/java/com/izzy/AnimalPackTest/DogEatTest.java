aa69838c9d990594e040c0b3f7e8d75a4c02a512
BugMan420 <ismaelsim@hotmail.co.uk>
2022-01-25 14:22:39 +0000

    DogEatTest has now been updated to run in jocomo

--- src/test/java/com/izzy/AnimalPackTest/DogEatTest.java
+++ src/test/java/com/izzy/AnimalPackTest/DogEatTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DogEatTest { @Test
-public void EatTest(){
+public void testEat(){
     Dog testDog = new Dog(5,5);
     testDog.eat();
     assertEquals(6,testDog.size,"The dog is the wrong size");
